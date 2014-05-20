62de28c275406e6e201b78367689c3ea085d67a0
xianhuifeng <xianhui.feng100@gmail.com>
2014-05-19 21:18:44 -0700

    getElements changed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,9 +1,24 @@
 // If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+/*var getElementsByClassName = function (className) {
+  return document.getElementsByClassName(className);
+};
+*/
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+  var results = [];
+
+  //get all ancestor nodes in the page
+
+  //recur start
+  
+  //in each ancestor node, check to see if has child node
+
+  	//if has not child, see if it has class name
+
+  		//if has, store that node into result
+  		//return
+
+  	//if has child, call recur(childnode)
+  return results;
 };
