a0072ce5016c3af386d255861919395b40302210
xianhuifeng <xianhui.feng100@gmail.com>
2014-05-20 22:01:45 -0700

    finish stringifyJSON

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -4,6 +4,7 @@ describe('stringifyJSON', function(){
 
     stringifiableObjects.forEach(function(test){
       var result = stringifyJSON(test);
+      console.log("Result is : ", result);
       var expected = JSON.stringify(test);
       expect(result).to.equal(expected);
     });
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,50 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+if(obj === null){
+	return "null";
+}
+
+if(typeof obj === "string"){
+	return "\""+ obj +"\"";
+}
+
+if(typeof obj  === "function"){
+	return "";
+}
+
+if(typeof obj === "undefined"){
+	return "";
+}
+
+if(typeof obj === "object" && obj.length !== undefined){
+	if(obj.length === 0){
+		return "["+"]";	
+	}else{
+		var subJson = "";
+		_.each(obj, function(value){
+			subJson += stringifyJSON(value) + ',';
+		});
+	    return "["+ subJson.slice(0, -1) +"]";
+	}
+}
+
+if(typeof obj === "object" && obj.length === undefined){
+	if(Object.keys(obj).length=== 0){
+		return "{"+"}";	
+	}else{
+		var subJson = "";
+		_.each(obj, function(value,key){
+			var subkey = stringifyJSON(key);
+			var subValue = stringifyJSON(value);
+			if(!subValue){
+				subJson = "";
+			}else{
+				subJson += subkey +':'+ subValue+',';
+			}
+		});
+	    return "{"+ subJson.slice(0, -1) +"}";
+	}
+}
+return (obj).toString();
 };
